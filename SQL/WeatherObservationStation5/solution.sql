a98447ed398006b4094f953d95942c6cf861876a
Miquel Vidal <miquel@miquelvidal.com>
2020-01-23 10:52:55 +0100

    Get the longest and shortest names

---
+++ SQL/WeatherObservationStation5/solution.sql
@@ -0,0 +1,12 @@
+/*
+  Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+*/
+(select city, char_length(city)
+from station
+order by char_length(city) asc,city asc
+LIMIT 1)
+union
+(select city, char_length(city)
+from station
+order by char_length(city) desc,city asc
+LIMIT 1)
