1d1a9284708057512f1150e3fcd9defe3b9d39d2
Miquel Vidal <miquel@miquelvidal.com>
2020-01-27 14:53:02 +0100

    Get different types in Scanner, take in account that String end with endline

---
+++ Java/ReadIntDoubleString/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        /*
+            as consider last scan don't end the line, then we have to skip end line
+            we make a second readline
+        */
+        String s = scan.nextLine();
+        s = scan.nextLine();
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
