d85e6e2236171224476d9c187239315c853a6c18
Miquel Vidal <miquel@miquelvidal.com>
2020-02-07 19:37:14 +0100

    At least only timeout :)

--- Java/MaxUniqueSubArrays/Solution.java
+++ Java/MaxUniqueSubArrays/Solution.java
@@ -3,9 +3,9 @@ public class test {
 
     public static int nouUnics(int[] Ms){
         int tmpUnic = 0;
-        for(int k=0;k<(Ms.length-1);k++){
+        for(int k=0;k<Ms.length;k++){
             int tmp1 = 1;
-            for(int l=0; (l<(Ms.length-1))&&(tmp1==1);l++){
+            for(int l=0; (l<Ms.length)&&(tmp1==1);l++){
                 if (l!=k){
                     if(Ms[k]==Ms[l]) tmp1=0;
                 }
@@ -20,14 +20,18 @@ public class test {
         int m = in.nextInt();
         int[] Ms = new int[m];
         int maxUnique = 0;
-        if (n<m) return;
+        if (n<m) {
+            System.out.println(0);
+            return;
+        }
         for (int i = 0; i < n; i++) {
             int num = in.nextInt();
             //la part inicial va a part, mentres no tenim prou fem cas especial
             if (i<m) {
                 Ms[i]=num;
                 //en concret quan no arriba ni a la talla minima no cal iterar
-                if (i<(m-1)) continue;
+                //if (i<(m-1)) continue;
+                //mentida... sembla que els petits els compten també
             }
             else{
                 //correm tots els elements
