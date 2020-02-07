2aaa7147a349e8bfe443d6d0c0e5a9dc80144d46
Miquel Vidal <miquel@miquelvidal.com>
2020-02-07 18:56:58 +0100

    Not working step, new approach

--- Java/MaxUniqueSubArrays/Solution.java
+++ Java/MaxUniqueSubArrays/Solution.java
@@ -1,28 +1,45 @@
 import java.util.*;
 public class test {
-    public static int calcularUnics(Deque<Integer> deque){
-        Iterator<Integer> iterador = deque.iterator();
-        Hashtable<String,Integer> valors = new Hashtable<String,Integer>();
-        while (iterador.hasNext()){
-            Integer tmp = iterador.next();
-            valors.put(""+tmp,tmp);
+
+    public static int nouUnics(int[] Ms){
+        int tmpUnic = 0;
+        for(int k=0;k<(Ms.length-1);k++){
+            int tmp1 = 1;
+            for(int l=0; (l<(Ms.length-1))&&(tmp1==1);l++){
+                if (l!=k){
+                    if(Ms[k]==Ms[l]) tmp1=0;
+                }
+            }
+            tmpUnic+=tmp1;
         }
-        return valors.size();
+        return tmpUnic;
     }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Deque<Integer> deque = new ArrayDeque<Integer>();
         int n = in.nextInt();
         int m = in.nextInt();
+        int[] Ms = new int[m];
         int maxUnique = 0;
         if (n<m) return;
         for (int i = 0; i < n; i++) {
             int num = in.nextInt();
-            deque.addLast(new Integer(num));
-            if (i<(m-1)) continue;
-            if (i>m) deque.removeFirst();
-            int newUnique = calcularUnics(deque);
-            if (maxUnique<newUnique) maxUnique= newUnique;
+            //la part inicial va a part, mentres no tenim prou fem cas especial
+            if (i<m) {
+                Ms[i]=num;
+                //en concret quan no arriba ni a la talla minima no cal iterar
+                if (i<(m-1)) continue;
+            }
+            else{
+                //correm tots els elements
+                for (int j=0;j<(m-1);j++){
+                    Ms[j]=Ms[j+1];
+                }
+                //afegim el nom al final
+                Ms[m-1]=num;
+            }
+            int unicLocal = nouUnics(Ms);
+            if (maxUnique<unicLocal) maxUnique = unicLocal;
+
         }
         System.out.println(maxUnique);
     }
