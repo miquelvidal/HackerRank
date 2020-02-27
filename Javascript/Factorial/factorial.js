6fcecefdd3d4b14654d3bae3216df829c29b6305
Miquel Vidal <miquel@miquelvidal.com>
2020-02-27 09:29:34 +0100

    Example function factorial (non-recursive)

---
+++ Javascript/Factorial/factorial.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+/*
+ * Create the function factorial here
+ */
+
+function factorial(entrada){
+    let numero = parseInt(entrada);
+    let i = numero;
+    let a = 1;
+    while(i!=0){
+        a=a*i;
+        i--;
+    }
+    return a;
+}
+
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}
