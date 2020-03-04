8975f4a52ea85d4591395e172883e87e05eaebbf
Miquel Vidal <miquel@miquelvidal.com>
2020-03-05 00:50:37 +0100

    Class Rectangle example

---
+++ Javascript/RectangleObject/solution.js
@@ -0,0 +1,31 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+class Rectangle {
+
+    constructor(l,w) {
+        this.length= l;
+        this.width = w;
+        this.perimeter = 2*(l+w);
+
