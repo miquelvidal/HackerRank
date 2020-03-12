a67e6a1c4128062be0af09418e3ee4c4678eb31a
Miquel Vidal <miquel@miquelvidal.com>
2020-03-12 07:48:34 +0100

    Simple example of function

---
+++ C/MaxLocal/maxLocal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d){
+    int max_local = a;
+    if (max_local<b) max_local = b;
+    if (max_local<c) max_local = c;
+    if (max_local<d) max_local = d;
+    return max_local;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
