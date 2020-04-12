568ccd7086d021a31618ea925135df184026ee8a
Miquel Vidal <miquel@miquelvidal.com>
2020-04-12 12:12:47 +0200

    Simple array sum in 2 total

---
+++ C/Array/genderMarks.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+//Complete the following function.
+
+int marks_summation(int* marks, int number_of_students, char gender) {
+  //Write your code here.
+  int i=0;
+  int boys = 0;
+  int girls = 0;
+  for(i=0;i<number_of_students;i++){
+      if (i%2==0) boys+=marks[i];
+      else girls+=marks[i];
+  }
+  if (gender=='b') return boys;
+  else return girls;
+}
+
+int main() {
+    int number_of_students;
+    char gender;
+    int sum;
+  
+    scanf("%d", &number_of_students);
+    int *marks = (int *) malloc(number_of_students * sizeof (int));
+ 
+    for (int student = 0; student < number_of_students; student++) {
+        scanf("%d", (marks + student));
+    }
+    
+    scanf(" %c", &gender);
+    sum = marks_summation(marks, number_of_students, gender);
+    printf("%d", sum);
+    free(marks);
+ 
+    return 0;
+}
