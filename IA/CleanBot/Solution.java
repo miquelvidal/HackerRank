1845516e3e3b53c5fd9d31180b2721372fa21e99
Miquel Vidal <miquel@miquelvidal.com>
2020-04-13 22:49:10 +0200

    Compila i mig funciona

--- IA/CleanBot/Solution.java
+++ IA/CleanBot/Solution.java
@@ -5,18 +5,20 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
+    /* al final opció més senzilla
     class MkPosition{
         public int x;
         public int y;
     }
+    */
     static void next_move(int posr, int posc, String[] board){
         //add logic here
         int joX = posc;
         int joY = posr;
-        int destiX = 0;
-        int destiY = 0;
+        int destiX = 999;
+        int destiY = 999;
         //si estem sobre brut no cal continua
-        if (board[joX].charAt(joY)=='d') {
+        if (board[joY].charAt(joX)=='d') {
             System.out.println("CLEAN");
             return;
         }
@@ -30,13 +32,38 @@ public class Solution {
         for (int y=0;y<board.length;y++){
             for(int x=0;x<5;x++){
                 if (board[y].charAt(x)=='d') {
-                   //MkPosition p = new MkPosition();
-                   //p.x=x;
-                   //p.y=y;
-                    destiX = destiX +()
+                    int difX = Math.abs(x-joX);
+                    int difY = Math.abs(y-joY);
+                    int dist = difX+difY;
+                    difX = Math.abs(destiX-joX);
+                    difY = Math.abs(destiY-joY);
+                    if((difX+difY)>dist){
+                        destiX=x;
+                        destiY=y;
+                    }
                 }
             }
         }
+        //Calculem el pas
+        //calculem els camins en els dos eixos
+        int camiX=destiX-joX;
+        int camiY=destiY-joY;
+        //positivitzem els valors
+        int tmpX=camiX*camiX;
+        int tmpY=camiY*camiY;
+        //per saber si verical o horitzontal comparem els valors positivitzats
+        //si X es més gran és horitzontal si no vertical
+        if (tmpX>tmpY){
+            //si es positiu és dreta, si no esquerra
+            if (camiX>0) System.out.println("RIGHT");
+            else System.out.println("LEFT");
+        }
+        else {
+            //si es positiu es cap a baix si no cap a d'alt
+            if (camiY>0) System.out.println("DOWN");
+            else System.out.println("UP");
+        }
+        
         
     }
 
